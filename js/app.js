ca8712e3b94fb0aa1a7942950dce0f41da0c1cb6
bw2648gg <72358607+bw2648gg@users.noreply.github.com>
2020-12-15 21:07:28 -0600

    adding function or background for list

--- js/app.js
+++ js/app.js
@@ -15,3 +15,15 @@ document.write('<h3>' + greeting + '</h3>');
 
 var $listHTML = $('ul').html();
 $('ul').append($listHTML);
+
+$(function() {
+    var backgroundColor = $('li').css('background-color');
+    $('ul').append('<p>Color was: ' + backgroundColor + '</p>');
+    $('li').css({
+        'background-color': '#c5a996',
+        'border': '1px solid #fff',
+        'color': '#000',
+        'font-family': 'Georgia',
+        'padding-left': '+=75'
+    });
+});
