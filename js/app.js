ddafeda0251987c2a49e59f0deeab14d04111f2c
bw2648gg <72358607+bw2648gg@users.noreply.github.com>
2020-12-16 15:21:05 -0600

    adding loops and gradient

--- js/app.js
+++ js/app.js
@@ -1,3 +1,32 @@
+var addToHead = document.querySelector('head');  
+    var cssLink = document.createElement('link'); 
+    addToHead.appendChild(cssLink);
+    cssLink.setAttribute('rel','stylesheet');
+    cssLink.setAttribute('href','css/style.css');
+
+    var wrapper = document.createElement('div');
+    wrapper.classList.add('wrapper');
+    var boDy = document.querySelector('body')
+    boDy.appendChild(wrapper);
+
+//for loop
+for (var i=0; i<4; i++){
+     var divs = document.createElement('div');
+     wrapper.appendChild(divs);
+     divs.classList.add('wrapperDivs');
+}
+//appendchild method and wrapper variable 
+
+
+//creating color variable
+var color1= "linear-gradient(135deg,#0B132B 0%, #3A506B 100%)";
+var color2= "linear-gradient(330deg, #5BC0BE 0%, #3A506B 100%)";
+var color3= "linear-gradient(35deg, #0B132B 0%, #5BC0BE 100%)";
+var color4= "linear-gradient(235deg, #5BC0BE 0%, #3A506B 100%)"; 
+
+
+
+
 var today = new Date();
 var hourNow = today.gethours();
 var greeting;
@@ -16,14 +45,3 @@ document.write('<h3>' + greeting + '</h3>');
 var $listHTML = $('ul').html();
 $('ul').append($listHTML);
 
-$(function() {
-    var backgroundColor = $('li').css('background-color');
-    $('ul').append('<p>Color was: ' + backgroundColor + '</p>');
-    $('li').css({
-        'background-color': '#c5a996',
-        'border': '1px solid #fff',
-        'color': '#000',
-        'font-family': 'Georgia',
-        'padding-left': '+=75'
-    });
-});
